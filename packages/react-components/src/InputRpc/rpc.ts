34467455bee8970e8e95020f73e73fcdedb60afc
qiuhao <qiuhao951@gmail.com>
2020-04-27 17:36:04 +0800

    lock @polkadot/api version

--- packages/react-components/src/InputRpc/rpc.ts
+++ packages/react-components/src/InputRpc/rpc.ts
@@ -1,6 +1,6 @@
 import jsonrpc from '@polkadot/types/interfaces/jsonrpc';
 import acalaJsonRpc from '@acala-network/types/interfaces/jsonrpc';
-import ormlJsonRpc from '@orml/types/interfaces/jsonrpc';
+import ormlJsonRpc from '@open-web3/orml-types/interfaces/jsonrpc'
 
 export default {
   ...jsonrpc,
